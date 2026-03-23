57d524bdc27bee19a72b19fe0c771d2e9362a5bd
Manh Thang <thangnmjob.com>
2026-03-23 11:27:29 +0700

    user chema

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import bodyParser from 'body-parser';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import { router } from './routes.js';
+import { connectDB } from './libs/db.js';
 
 dotenv.config();
 
@@ -16,7 +13,9 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json());
 
 app.use(cors());
-
-app.listen(PORT, () => {
+connectDB().then(() => {
+    app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
+})
+
